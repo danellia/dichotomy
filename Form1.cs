3698b1f77b519e6573a1f6dd18939f4f8b0cebfb
danellia <deshkadary2@gmail.com>
2021-09-23 12:56:32 +0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+//эпсилон 10^-3, 10^-6
+//подкрутить precision
+//сообщения для NaN
+//delete extra usings
+//какой шаг?
+namespace dichotomy
+{
+    public partial class Form1 : Form
+    {
+        GraphPane pane;
+        public Form1()
+        {
+            InitializeComponent();
+            pane = zedGraphControl.GraphPane;
+            pane.Title.Text = "Графики";
+        }
+        private async void countToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UserExpression expression = new UserExpression()
+                {
+                    a = Convert.ToDouble(textBoxA.Text),
+                    b = Convert.ToDouble(textBoxB.Text),
+                    precision = Convert.ToDouble(textBoxE.Text),
+                    userExpression = textBoxExpression.Text
+                };
+
+                Task<PointPairList> getPoints = expression.getGraphPoints();
+                LineItem curve = pane.AddCurve(expression.userExpression, await getPoints, Color.Purple, SymbolType.None);
+                textBoxMinPointY.Text = expression.findMinPoint().ToString();
+                zedGraphControl.AxisChange();
+                zedGraphControl.Refresh();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Заполните пустые поля!");
+            }
+        }
+        private void clearAllStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clearValueTextBoxes();
+            clearMinPointTextBoxes();
+            clearGraph();
+        }
+        private void clearGraphtoolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clearMinPointTextBoxes();
+            clearGraph();
+        }
+        private void clearValuesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            clearValueTextBoxes();
+            clearMinPointTextBoxes();
+        }
+        private void clearValueTextBoxes()
+        {
+            textBoxA.Clear();
+            textBoxB.Clear();
+            textBoxE.Clear();
+            textBoxExpression.Clear();
+        }
+        private void clearMinPointTextBoxes()
+        {
+            textBoxMinPointX.Clear();
+            textBoxMinPointY.Clear();
+        }
+        private void clearGraph()
+        {
+            pane.CurveList.Clear();
+            zedGraphControl.Refresh();
+        }
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void checkDoubleInput(TextBox textBox, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '.' && textBox.Text.IndexOf('.') != -1)
+            {
+                e.Handled = true;
+                return;
+            }
+            if (!char.IsDigit(e.KeyChar) && e.KeyChar != 8 && e.KeyChar != '.' && e.KeyChar != '-')
+            {
+                e.Handled = true;
+            }
+        }
+        private void textBoxA_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            checkDoubleInput(textBoxA, e);
+        }
+        private void textBoxB_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            checkDoubleInput(textBoxB, e);
+        }
+        private void textBoxE_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            checkDoubleInput(textBoxE, e);
+        }
+    }
+}
---
+++ UserExpression.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using org.mariuszgromada.math.mxparser;
+using ZedGraph;
+
+namespace dichotomy
+{
+    class UserExpression
+    {
+        public double a { get; set; }
+        public double b { get; set; }
+        public double precision { get; set; }
+        public string userExpression { get; set; }
+        public PointPairList pointList { get; set; }
+        public double step = 1;
+
+        public Task<PointPairList> getGraphPoints()
+        {
+            //expression.checkSyntax();
+            //expression.getErrorMessage();
+            pointList = new PointPairList();
+            return Task.Run(() => {
+                for (double pointX = a; pointX <= b; pointX += step)
+                {
+                    double pointY = getPointY(pointX);
+                    pointList.Add(new PointPair(pointX, pointY));
+                    Debug.WriteLine(String.Format("x={0}, y={1}", pointX, pointY));
+                }
+                return pointList;
+            });
+        }
+
+        private double getPointY(double pointX)
+        {
+            Argument x = new Argument("x = " + pointX);
+            Expression expression = new Expression(userExpression, x);
+            double pointY = expression.calculate();
+            return pointY;
+        }
+
+        public double findMinPoint()
+        {
+            //ищет макс вместо мин
+            double leftX = a;
+            double rightX = b;
+            double leftY = getPointY(a);
+            double rightY = getPointY(b);
+            double middleX = (leftX + rightX) / 2;
+            double middleY = getPointY(middleX);
+            while (rightX - leftX >= precision)
+            {
+                Debug.WriteLine(String.Format("rightX - leftX={0}", rightX - leftX));
+                if (leftY * middleY < 0)
+                {
+                    leftX = middleX;
+                }
+                else
+                {
+                    rightX = middleX;
+                }
+                middleX = (leftX + rightX) / 2;
+                middleY = getPointY(middleX);
+                Debug.WriteLine(String.Format("middleX={0}, middleY={1}", middleX, middleY));
+            }
+            return middleY;
+        }
+    }
+}
